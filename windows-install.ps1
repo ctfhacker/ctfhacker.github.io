7af11612844ed9fffd1f1430c2f62b16aed36e9a
thebarbershopper <cld251@gmail.com>
2017-01-15 00:06:52 -0600

    Switch to cmder

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,4 +1,8 @@
 Set-ExecutionPolicy Unrestricted
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y conemu
-choco install -y msys2
+choco install -y cmder
+
+choco install -y git
+git clone https://github.com/ctfhacker/cmder-dotfiles %HOME%/dotfiles
+cd %HOME%/dotfiles
+install.bat
