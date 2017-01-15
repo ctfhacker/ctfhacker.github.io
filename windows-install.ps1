3597f3fce2a57d1e89eca1a8397631fc1f8b50ba
thebarbershopper <cld251@gmail.com>
2017-01-15 00:24:24 -0600

    Admin windows-install

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Unrestricted
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y cmder
-
 choco install -y git.install
