0ce234294b035d19c4996345db2f11ac0565333f
thebarbershopper <cld251@gmail.com>
2017-01-14 22:21:58 -0600

    Add conemu and msys2 via chocolatey

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,4 +1,4 @@
 Set-ExecutionPolicy Unrestricted
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install conemu
-choco install msys2
+choco install -y conemu
+choco install -y msys2
