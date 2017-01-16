84b49c7dd30e9058d1c36edb045481294061d1c0
thebarbershopper <cld251@gmail.com>
2017-01-16 16:00:07 -0600

    Changing the structure of windows-install to pull scripts from a different repo

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,30 +1,14 @@
+Write-Host "Installing chocolatey"
 Set-ExecutionPolicy Unrestricted
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y windbg
-choco install -y cmder
-choco install -y git.install
-choco install -y vcredist2005
-choco install -y vcredist2008
-choco install -y vcredist2010
-choco install -y vcredist2012
-choco install -y vcredist2013
-choco install -y vcredist2015
-choco install -y googlechrome
-choco install -y dotnet3.5
-choco install -y dotnet4.5.2
-choco install -y dotnet4.6.1
-choco install -y 7zip.commandline
-choco install -y curl
-choco install -y git
-choco install -y python2
-choco install -y sysinternals
-choco install -y wget
-choco install -y windows-sdk-8.1
-choco install -y windows-sdk-10.1
-choco install -y visualstudio2012professional
-choco install -y cmake
 
-pip2 install pykd
-pip3 install pykd
-((new-object system.net.webclient).downloadfile('https://github.com/ctfhacker/windows-dotfiles/raw/master/pykd-dlls/x86/pykd.dll', 'C:\Program Files\Windows Kits\8.1\Debuggers\x86\winext\pykd.dll'))
-((new-object system.net.webclient).downloadfile('https://github.com/ctfhacker/windows-dotfiles/raw/master/pykd-dlls/x64/pykd.dll', 'C:\Program Files\Windows Kits\8.1\Debuggers\x64\winext\pykd.dll'))
+Write-Host "Installing git"
+choco install -y git.install
+
+Write-Host "Cloning windows-setup"
+git clone https://github.com/ctfhacker/windows-setup C:\Windows\Temp\
+
+Get-ChildItem 'C:\Windows\Temp\windows-setup' | ForEach-Object {
+    Write-Host "Executing " + $_.FullName
+    & $_.FullName
+}
