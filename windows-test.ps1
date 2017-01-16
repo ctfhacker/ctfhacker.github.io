0a27d2886857fdbd14d9e41ca419d21fede8f031
thebarbershopper <cld251@gmail.com>
2017-01-16 00:14:48 -0600

    more tests

--- windows-test.ps1
+++ windows-test.ps1
@@ -1,1 +1,1 @@
-iex ((new-object system.net.webclient).downloadfile('https://github.com/ctfhacker/windows-dotfiles/raw/master/pykd-dlls/x86/pykd.dll', 'C:\pykd.dll'))
+((new-object system.net.webclient).downloadfile('https://github.com/ctfhacker/windows-dotfiles/raw/master/pykd-dlls/x86/pykd.dll', 'C:\pykd.dll'))
