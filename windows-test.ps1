2b7cc95adc26dc163e38ffbd0fd9ad602f991bed
thebarbershopper <cld251@gmail.com>
2017-01-16 00:16:30 -0600

    more tests

--- windows-test.ps1
+++ windows-test.ps1
@@ -1,1 +1,2 @@
-((new-object system.net.webclient).downloadfile('https://github.com/ctfhacker/windows-dotfiles/raw/master/pykd-dlls/x86/pykd.dll', 'C:\pykd.dll'))
+((new-object system.net.webclient).downloadfile('https://github.com/ctfhacker/windows-dotfiles/raw/master/pykd-dlls/x86/pykd.dll', 'C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll'))
+((new-object system.net.webclient).downloadfile('https://github.com/ctfhacker/windows-dotfiles/raw/master/pykd-dlls/x64/pykd.dll', 'C:\Program Files\Windows Kits\10\Debuggers\x64\winext\pykd.dll'))
