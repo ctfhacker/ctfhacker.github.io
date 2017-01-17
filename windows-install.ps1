fb15f7145634bec4e1fb652a11d56719998d77f1
thebarbershopper <cld251@gmail.com>
2017-01-16 22:21:01 -0600

    Git isn't quite working, testing downloading zip of source

--- windows-install.ps1
+++ windows-install.ps1
@@ -8,7 +8,12 @@ choco install -y git.install
 Write-Host "Cloning windows-setup"
 git clone https://github.com/ctfhacker/windows-setup C:\Windows\Temp\windows-setup
 
-cd C:\Windows\Temp\windows-setup
+((New-Object System.Net.WebClient).DownloadFile('https://github.com/ctfhacker/windows-setup/archive/master.zip', 'C:\Windows\Temp\master.zip'))
+
+cd C:\Windows\Temp\
+[System.IO.Compression.ZipFile]::ExtractToDirectory('master.zip', 'windows-setup')
+cd windows-setup
+
 Get-ChildItem | ForEach-Object {
     Write-Host "Executing " + $_.FullName
     & $_.FullName
