99fb2b08cc6c495aef95cec92e50ffe9ef348123
thebarbershopper <cld251@gmail.com>
2017-01-18 12:50:08 -0600

    Use unzip instead of .NET

--- windows-install.ps1
+++ windows-install.ps1
@@ -2,14 +2,14 @@ Write-Host "Installing chocolatey"
 Set-ExecutionPolicy Unrestricted
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-Write-Host "Installing git"
-choco install -y git.install
+Write-Host "Installing unzip"
+choco install -y unzip
 
 ((New-Object System.Net.WebClient).DownloadFile('https://github.com/ctfhacker/windows-setup/archive/master.zip', 'C:\Windows\Temp\master.zip'))
 
 cd C:\Windows\Temp\
-[System.IO.Compression.ZipFile]::ExtractToDirectory('master.zip', 'windows-setup')
-cd windows-setup
+unzip master.zip
+cd windows-setup-master
 
 Get-ChildItem | ForEach-Object {
     Write-Host "Executing " + $_.FullName
