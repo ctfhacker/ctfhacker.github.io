c7ed40e6900ab44cbe765bc51600653ae10de9a7
thebarbershopper <cld251@gmail.com>
2017-01-21 22:33:46 -0600

    windows test files

--- windows-test.ps1
+++ windows-test.ps1
@@ -1,2 +1,2 @@
-choco install virtualclonedrive
+choco install -y virtualclonedrive
 ((new-object system.net.webclient).downloadfile('http://download.microsoft.com/download/1/E/5/1E5F1C0A-0D5B-426A-A603-1798B951DDAE/VS2010Express1.iso', 'C:\Windows\Temp\VS2010Express.iso'))
