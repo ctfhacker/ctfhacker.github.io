ec320ad6a42598a7922dd6c70527878b40cadda0
thebarbershopper <cld251@gmail.com>
2017-01-21 22:59:23 -0600

    Derp on filepath to mount.exe

--- windows-test.ps1
+++ windows-test.ps1
@@ -4,5 +4,8 @@ if(-Not (Get-Command choco -errorAction SilentlyContinue)){
 choco install -y virtualclonedrive
 Write-Host "Downloading VS2010Express.iso" -foregroundcolor "Green" -backgroundcolor "Black"
 ((new-object system.net.webclient).downloadfile('http://download.microsoft.com/download/1/E/5/1E5F1C0A-0D5B-426A-A603-1798B951DDAE/VS2010Express1.iso', 'C:\Windows\Temp\VS2010Express.iso'))
-& "C:\Program Files\Elaborate Bytes\Virtual CloneDrive\VCDMount.exe" /l=E C:\Windows\Temp\VS2010Express.iso
+Write-Host "Mounting VS2010Express.iso" -foregroundcolor "Green" -backgroundcolor "Black"
+& "C:\Program Files\Elaborate Bytes\VirtualCloneDrive\VCDMount.exe" /l=E C:\Windows\Temp\VS2010Express.iso
+Write-Host "Installing VS2010Express.iso" -foregroundcolor "Green" -backgroundcolor "Black"
 E:\VCExpress\setup.exe /q /norestart
+Write-Host "Done Installing VS2010Express.iso" -foregroundcolor "Green" -backgroundcolor "Black"
